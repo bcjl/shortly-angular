8c00c6716d6976dc3e7443d66fc146f24a74f348
bcjl <briancjliu@gmail.com>
2015-04-22 16:22:20 -0700

    Completed LinksController, passing tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,15 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data={};
+  $scope.data.links=[];
+  $scope.init = function(){
+    this.getLinks();
+  };
+  $scope.getLinks = function(){
+    $scope.data.links=Links.getLinks().then(function(links){
+      $scope.data.links=links;
+    });
+  };
+  $scope.init();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,9 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.url = '';
+
+  $scope.submit = function(){
+    console.log('LOGGED: ',$scope.url);
+  };
 });
